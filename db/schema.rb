78c3d2e784a3e38be6492fe508ab480c946a1576
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-04-06 18:51:01 -0400

    Add Event scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_201336) do
+ActiveRecord::Schema.define(version: 2021_04_06_225004) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

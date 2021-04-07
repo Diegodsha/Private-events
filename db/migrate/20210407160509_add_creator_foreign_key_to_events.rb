bfe85c1cb74cf22a62b22e975a8128df79884608
Diego <diegodsha@hotmail.com>
2021-04-07 11:21:43 -0500

    Show event on user show view

---
+++ db/migrate/20210407160509_add_creator_foreign_key_to_events.rb
@@ -0,0 +1,6 @@
+class AddCreatorForeignKeyToEvents < ActiveRecord::Migration[6.1]
+  def change
+    add_column :events, :creator_id, :integer, null:false
+    add_foreign_key :events, :users, column: :creator_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_225004) do
+ActiveRecord::Schema.define(version: 2021_04_07_160509) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_225004) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_04_06_225004) do
     t.string "email"
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
